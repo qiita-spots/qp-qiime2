b506095573f65dd7f7002f78845591066a10dd3a
Antonio Gonzalez <antgonza@gmail.com>
2017-07-05 09:47:43 -0600

    adding alpha

--- qp_qiime2/__init__.py
+++ qp_qiime2/__init__.py
@@ -9,7 +9,7 @@
 
 from qiita_client import QiitaPlugin, QiitaCommand
 
-from .qiime2 import rarefy, beta_diversity, pcoa
+from .qiime2 import rarefy, beta_diversity, pcoa, alpha_diversity
 from qiime2 import __version__ as qiime2_version
 
 
@@ -68,5 +68,33 @@ dflt_param_set = {
 qiime_cmd = QiitaCommand(
     "pcoa", "Principal Coordinate Analysis",
     pcoa, req_params, opt_params, outputs, dflt_param_set,
+    analysis_only=True)
+plugin.register_command(qiime_cmd)
+
+# Define the alpha command
+req_params = {'i-table': ('artifact', ['BIOM'])}
+opt_params = {
+    'p-metric': [
+        ('choice:["simpson", "enspie", "doubles", "mcintosh_d", "chao1", '
+         '"kempton_taylor_q", "observed_otus", "singles", "strong", '
+         '"robbins", "menhinick", "osd", "shannon", "brillouin_d", '
+         '"margalef", "lladser_ci", "pielou_e", "michaelis_menten_fit", '
+         '"ace", "chao1_ci", "goods_coverage", "esty_ci", "fisher_alpha", '
+         '"berger_parker_d", "heip_e", "simpson_e", "dominance", '
+         '"mcintosh_e", "lladser_pe", "gini_index", "faith_pd"]'),
+        'observed_otus'],
+    'i-tree': ['choice:["default", "None"]', 'None']}
+#
+# change!
+#
+outputs = {'o-alpha-diversity': 'ordination_results'}
+dflt_param_set = {
+    'Defaults': {
+        'p-metric': 'observed_otus',
+        'i-tree': 'None'}
+}
+qiime_cmd = QiitaCommand(
+    "alpha_diversity", "Alpha Diversity",
+    alpha_diversity, req_params, opt_params, outputs, dflt_param_set,
     analysis_only=True)
 plugin.register_command(qiime_cmd)
--- qp_qiime2/tests/test_qiime2.py
+++ qp_qiime2/tests/test_qiime2.py
@@ -19,7 +19,7 @@ from qiita_client.testing import PluginTestCase
 from qiime2 import __version__ as qiime2_version
 
 from qp_qiime2 import plugin
-from qp_qiime2.qiime2 import (rarefy, beta_diversity, pcoa)
+from qp_qiime2.qiime2 import (rarefy, beta_diversity, pcoa, alpha_diversity)
 
 
 class qiime2Tests(PluginTestCase):
@@ -216,6 +216,89 @@ class qiime2Tests(PluginTestCase):
         # and that element [0] should have this file
         exp = [(join(out_dir, 'pcoa/pcoa/ordination.txt'), 'plain_text')]
         self.assertEqual(ainfo[0].files, exp)
+
+    def test_alpha(self):
+        out_dir = mkdtemp()
+        self._clean_up_files.append(out_dir)
+
+        # qiime2 currently only works with rarefied tables so we need to
+        # rarefy it
+        params = {'p-sampling-depth': 10, 'i-table': 5}
+        data = {'user': 'demo@microbio.me',
+                'command': dumps(['qiime2', qiime2_version, 'Rarefy']),
+                'status': 'running',
+                'parameters': dumps(params)}
+        jid = self.qclient.post('/apitest/processing_job/', data=data)['job']
+        success, ainfo, msg = rarefy(self.qclient, jid, params, out_dir)
+        data = {'filepaths': dumps(ainfo[0].files), 'type': "BIOM",
+                'name': "Rarefied biom", 'analysis': 1, 'data_type': '16S'}
+        reply = self.qclient.post('/apitest/artifact/', data=data)
+        aid = reply['artifact']
+
+        # actually test non phylogenetic beta diversity
+        params = {
+            'i-table': aid, 'p-metric': 'observed_otus',
+            'i-tree': 'None'}
+        data = {'user': 'demo@microbio.me',
+                'command': dumps(['qiime2', qiime2_version, 'beta_diversity']),
+                'status': 'running',
+                'parameters': dumps(params)}
+        jid = self.qclient.post('/apitest/processing_job/', data=data)['job']
+        success, ainfo, msg = alpha_diversity(
+            self.qclient, jid, params, out_dir)
+        self.assertEqual(msg, '')
+        self.assertTrue(success)
+        # only 1 element
+        self.assertEqual(len(ainfo), 1)
+        # and that element [0] should have this file
+        exp = [(join(out_dir, 'alpha_diversity/alpha/alpha-diversity.tsv'),
+                'plain_text')]
+        self.assertEqual(ainfo[0].files, exp)
+
+        params['p-metric'] = 'faith_pd'
+        params['i-tree'] = join(
+            dirname(realpath(__file__)), 'prune_97_gg_13_8.tre')
+        jid = self.qclient.post('/apitest/processing_job/', data=data)['job']
+        success, ainfo, msg = alpha_diversity(
+            self.qclient, jid, params, out_dir)
+        self.assertEqual(msg, '')
+        self.assertTrue(success)
+        # only 1 element
+        self.assertEqual(len(ainfo), 1)
+        # and that element [0] should have this file
+        exp = [(join(out_dir, 'alpha_diversity/alpha/alpha-diversity.tsv'),
+                'plain_text')]
+        self.assertEqual(ainfo[0].files, exp)
+
+        # To avoid having to set up all these files, we are gonna test
+        # that if phylogentic and no tree it fails
+        params['i-tree'] = None
+        jid = self.qclient.post('/apitest/processing_job/', data=data)['job']
+        success, ainfo, msg = alpha_diversity(
+            self.qclient, jid, params, out_dir)
+        self.assertFalse(success)
+        self.assertEqual(msg, 'Phylogentic metric faith_pd selected '
+                              'but no tree exists')
+
+    def test_alpha_errors(self):
+        out_dir = mkdtemp()
+        self._clean_up_files.append(out_dir)
+
+        # no rarefied - this testes that the non rarefied table conversion
+        # works and that if qiime fails it raises the correct error_msg
+        params = {
+            'i-table': 5, 'p-metric': 'observed_otus', 'i-tree': None}
+        data = {'user': 'demo@microbio.me',
+                'command': dumps(['qiime2', qiime2_version, 'beta_diversity']),
+                'status': 'running',
+                'parameters': dumps(params)}
+        jid = self.qclient.post('/apitest/processing_job/', data=data)['job']
+        success, ainfo, msg = alpha_diversity(
+            self.qclient, jid, params, out_dir)
+
+        self.assertIn("Argument to input 'table' is not a subtype of "
+                      "FeatureTable[Frequency]", msg)
+        self.assertFalse(success)
 
 
 if __name__ == '__main__':
