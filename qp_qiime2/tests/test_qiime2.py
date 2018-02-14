703b7809b6e5e85387edb7a338488620820655ea
Antonio Gonzalez <antgonza@gmail.com>
2018-02-14 09:35:51 -0700

    adding tests

--- qp_qiime2/tests/test_qiime2.py
+++ qp_qiime2/tests/test_qiime2.py
@@ -150,10 +150,20 @@ class qiime2Tests(PluginTestCase):
 
         # To avoid having to set up all these files, we are gonna test
         # that if phylogenetic and no tree it fails
-        params['Phylogenetic tree'] = "None"
+        params['Diversity metric'] = 'Euclidean distance'
         jid = self.qclient.post('/apitest/processing_job/', data=data)['job']
         success, ainfo, msg = beta_diversity(
             self.qclient, jid, params, out_dir)
+        self.assertFalse(success)
+        self.assertEqual(msg, 'Error. Metric: euclidean (is '
+                              'phylogenetic: %s), tree: None' %
+                              params['Phylogenetic tree'])
+
+        params['Phylogenetic tree'] = "None"
+        params['Diversity metric'] = 'Unweighted UniFrac'
+        jid = self.qclient.post('/apitest/processing_job/', data=data)['job']
+        success, ainfo, msg = beta_diversity(
+          self.qclient, jid, params, out_dir)
         self.assertFalse(success)
         self.assertEqual(msg, 'Error. Metric: unweighted UniFrac (is '
                               'phylogenetic: True), tree: None')
@@ -349,7 +359,17 @@ class qiime2Tests(PluginTestCase):
 
         # To avoid having to set up all these files, we are gonna test
         # that if phylogenetic and no tree it fails
+        params['Diversity metric'] = 'Number of distinct features'
+        jid = self.qclient.post('/apitest/processing_job/', data=data)['job']
+        success, ainfo, msg = beta_diversity(
+            self.qclient, jid, params, out_dir)
+        self.assertFalse(success)
+        self.assertEqual(msg, 'Error. Metric: observed_otus (is '
+                              'phylogenetic: %s), tree: None' %
+                              params['Phylogenetic tree'])
+
         params['Phylogenetic tree'] = "None"
+        params['Diversity metric'] = "Faith's Phylogenetic Diversity"
         jid = self.qclient.post('/apitest/processing_job/', data=data)['job']
         success, ainfo, msg = alpha_diversity(
             self.qclient, jid, params, out_dir)
