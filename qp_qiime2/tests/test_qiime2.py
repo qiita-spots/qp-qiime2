6aa8bfa63124cd1aba06e703439e016a828e03ba
Antonio Gonzalez <antgonza@gmail.com>
2022-10-31 09:44:01 -0600

    fix filter_features missing metadata issue

--- qp_qiime2/tests/test_qiime2.py
+++ qp_qiime2/tests/test_qiime2.py
@@ -1246,6 +1246,9 @@ class qiime2Tests(PluginTestCase):
             'feature table will be retained. (where)': '',
             'The feature table from which features should be filtered. '
             '[table]': '8',
+            'qp-hide-metadata': 'Feature metadata used with `where` parameter '
+            'when selecting features to retain, or with `exclude_ids` when '
+            'selecting features to discard. (metadata)',
             'Feature metadata used with `where` parameter when selecting '
             'features to retain, or with `exclude_ids` when selecting '
             'features to discard. (metadata)': '',
